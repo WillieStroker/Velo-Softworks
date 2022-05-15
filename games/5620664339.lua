2023ecca4839b07473f2894ebe09e64e10084476
WillieStroker <105597056+WillieStroker@users.noreply.github.com>
2022-05-15 23:15:48 +0200

    Update 5620664339.lua

--- games/5620664339.lua
+++ games/5620664339.lua
@@ -1,1 +1,36 @@
-print("RoStreets")
+local owner = game.CreatorId
+local developers = {
+  [154069155] = "plainsour",
+  [74804286] = "KingBuggy002",
+  [51789409] = "yanrr_002"
+}
+
+local players = game:GetService("Players")
+local player = players.LocalPlayer
+
+for _, v in pairs(players:GetChildren()) do
+    if v.UserId == owner then
+        game.StarterGui:SetCore("SendNotification", {
+            Title = "Warning",
+            Text = "found owner in server, be careful!",
+            Duration = 5
+        })
+    end
+  
+    for _2, v2 in next, developers do
+        if v.UserId == _2 then
+            warn("Found a developer in server, be careful! Developer:"..v2)
+        end
+    end
+end
+
+game.Players.PlayerAdded:Connect(function(player)
+    if player.UserId == owner then
+        game.StarterGui:SetCore("SendNotification", {
+            Text = "Warning",
+            Content = "Owner joined server, be careful!",
+            Icon = "",
+            Duration = 5
+        })
+    end
+end)
