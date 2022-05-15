d1d8b9edf45c054a084a3851cd4e584be5f2248f
WillieStroker <105597056+WillieStroker@users.noreply.github.com>
2022-05-15 17:13:01 +0200

    Update 7621012516.lua

--- games/7621012516.lua
+++ games/7621012516.lua
@@ -1,1 +1,15 @@
-print("You are gay")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Velo", HidePremium = true, SaveConfig = false, ConfigFolder = "Velo"})
+
+local AutoFarmTab = Window:MakeTab({
+	Name = "Auto Farm",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
