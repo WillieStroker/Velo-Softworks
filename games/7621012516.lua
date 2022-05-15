9405f366361ee105d4b6ef31a3687bbdf38d7564
WillieStroker <105597056+WillieStroker@users.noreply.github.com>
2022-05-15 22:47:16 +0200

    Update 7621012516.lua

--- games/7621012516.lua
+++ games/7621012516.lua
@@ -1,1 +1,14 @@
+local ownerId = game.CreatorId
+local players = game:GetService("Players")
 
+for _, v in pairs(players:GetChildren()) do
+  if v.UserId == ownerId then
+    print("Owner is in game, be careful")
+  end
+end
+
+game.Players.PlayerAdded:Connect(function(player)
+    if player.UserId == ownerId then
+        print("got the owner!")
+    end
+end)
