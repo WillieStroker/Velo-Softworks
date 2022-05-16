b0088716bfe519dbc27e2d9c82448dd3e7cd3c91
WillieStroker <105597056+WillieStroker@users.noreply.github.com>
2022-05-16 16:18:11 +0200

    Update 7621012516.lua

--- games/7621012516.lua
+++ games/7621012516.lua
@@ -1,7 +1,1 @@
-local g = pcall(function()
-    loadstring(game:HttpGet("https://pst.klgrth.io/paste/az28vv9rbuxypspwdfoxb4sw",true))()
-end
-  
-if g then
-    game.Players.LocalPlayer:Kick("Owner in game")
-end
+print("Thug shakers are the best")
